be614cc644063beee87ca59c1b2cbec99dffcbd5
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-08-21 07:08:51 +0200

    vim keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,22 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Keep things vertically centered during searches
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- copy paste:
+-- replace selection without overwriting unamed register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- yank synchorinized with clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Move lines in visual mode
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
